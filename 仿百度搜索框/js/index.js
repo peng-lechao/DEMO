98b3ba36e512d2abdcdbdeda032a2023b8ba8f47
peng-lechao <543826037@qq.com>
2017-10-01 15:54:12 +0800

    10.1

--- 仿百度搜索框/js/index.js
+++ 仿百度搜索框/js/index.js
@@ -1,36 +1,31 @@
 //创建对象
-class SearchBtn {
+class Base {
 	// 构造函数
-	constructor(id='searchText',btn='btn',area='area'){
-		this.id = document.getElementById(id);
-		this.btn = document.getElementById(btn);
-		this.area = document.getElementById(area);
+	constructor(data){
+		this.data = data;
+		this.bind();
 	}
-	// 输出数据
-	output(data){  
-	  // 调用删除节点函数
-	  this.deleteNodes();
-	  let len = data.s.length;
-	  // 循环输出data.s里面的数据，创造节点，插入数据
-	  if(len>0){
-	  	  this.area.style.display='block';
-		  for(let i=0;i<data.s.length;i++){
-		  	let li = document.createElement('li'),
-		  		link = document.createElement('a');
-		  	link.innerHTML = data.s[i];
-		  	link.href = 'https://www.baidu.com/s?wd='+data.s[i];
-		  	li.appendChild(link);
-		  	this.area.appendChild(li);
-		  }
-	  }else this.area.style.display='none';
+
+	get(key){
+		return this.data[key];
 	}
-	// 输入框键入时事件
-	inputChange(){
+}
+
+class SearchBtn extends Base{
+	bind(){
+		let self = this;
+		self.get('searchText').addEventListener('input',function(){
+			self.change();
+		});
+	}
+
+	change(){
+		let value = this.get('searchText').value;
 		// 获取输入框数据，加入地址中
-		let location = 'https://www.baidu.com/s?wd='+this.id.value;
-		this.btn.href = location;
+		let location = 'https://www.baidu.com/s?wd='+value;
+		this.get('btn').href = location;
 		// 使用百度的搜索地址
-		let	src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd="+this.id.value+"&cb=search.output";
+		let	src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd="+value+"&cb=search.output";
 		// 创造新的script节点
 		let addScript = document.getElementsByTagName('script'),
 			script = document.createElement('script');
@@ -41,22 +36,44 @@ class SearchBtn {
 			document.body.removeChild(addScript[1]);
 		}
 		// 当输入框为空时，删除节点，隐藏信息框
-		if(this.id.value==''){
-			this.area.style.display = 'none';
+		if(value==''){
+			this.get('area').style.display = 'none';
 	  		this.deleteNodes();
 		}else {
 			document.body.appendChild(script);
 		}
 	}
-	// 循环删除之前节点
+
+	output(data){  
+	  // 调用删除节点函数
+	  this.deleteNodes();
+	  let len = data.s.length;
+	  // 循环输出data.s里面的数据，创造节点，插入数据
+	  if(len>0){
+	  	  this.get('area').style.display='block';
+		  for(let i=0;i<data.s.length;i++){
+		  	let li = document.createElement('li'),
+		  		link = document.createElement('a');
+		  	link.innerHTML = data.s[i];
+		  	link.href = 'https://www.baidu.com/s?wd='+data.s[i];
+		  	li.appendChild(link);
+		  	this.get('area').appendChild(li);
+		  }
+	  }else this.get('area').style.display='none';
+	}
+
 	deleteNodes(){
-		for(let i=this.area.childNodes.length-1;i>=0;i--){
-	 		this.area.removeChild(this.area.childNodes[i]);
-	  	}
+		for(let i=this.get('area').childNodes.length-1;i>=0;i--){
+ 			this.get('area').removeChild(this.get('area').childNodes[i]);
+  		}
 	}
 }
 
-let search = new SearchBtn();
-search.id.oninput = search.inputChange.bind(search);
+let data = {
+	searchText:document.getElementById("searchText"),
+	btn:document.getElementById("btn"),
+	area:document.getElementById("area")
+};
+let search = new SearchBtn(data);
 
 
